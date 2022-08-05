4c462eff8830e6c9478c58903cfa5df9169a9195
cisarLZamora <leazamorace@outlook.com>
2022-08-05 18:46:10 -0500

    Ejercicio de practica
    Signed-off-by: cisarLZamora <leazamorace@outlook.com>

--- chess.js
+++ chess.js
@@ -21,3 +21,11 @@ funcion que tarde 15 segundos en dar una respuesta de cualquier tipo
 calcular(10,5, imprimir,15000);
 
 console.log("lo que sea");
+
+/*
+INVESTIFGAR FUNCIÓN FOREACH EN ARRAYS 
+investigar parametros (cuantos necesita)
+qué son cada parametro
+hacer un ejemplo con una funcion anonima
+y un ejemplo con una funcion declarada pasandolo como referencia
+*/
